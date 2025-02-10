0974b37a18454fe9d633eb328e331fec4f3c95eb
kangjuhyup <fog0510@gmail.com>
2025-02-10 15:38:03 +0900

    test(common): add tests for parallel option metadata registration

--- packages/common/test/decorators/use-guards.decorator.spec.ts
+++ packages/common/test/decorators/use-guards.decorator.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { GUARDS_METADATA } from '../../constants';
+import { GUARDS_METADATA, GUARDS_OPTION_METADATA } from '../../constants';
 import { UseGuards } from '../../decorators/core/use-guards.decorator';
 import { InvalidDecoratorItemException } from '../../utils/validate-each.util';
 
@@ -43,5 +43,42 @@ describe('@UseGuards', () => {
     } catch (e) {
       expect(e).to.be.instanceof(InvalidDecoratorItemException);
     }
+  });
+});
+
+describe('@UseGuards with parallel option', () => {
+  it('should register parallel option metadata on class when parallel is true', () => {
+    @UseGuards(Guard, { parallel: true })
+    class TestParallelClass {}
+    
+    const metadata = Reflect.getMetadata(GUARDS_OPTION_METADATA, TestParallelClass);
+    expect(metadata).to.be.eql([true]);
+  });
+
+  it('should register parallel option metadata on method when parallel is true', () => {
+    class TestParallelMethod {
+      @UseGuards(Guard, { parallel: true })
+      public static test() {}
+    }
+    const metadata = Reflect.getMetadata(GUARDS_OPTION_METADATA, TestParallelMethod.test);
+    expect(metadata).to.be.eql([true]);
+  });
+
+  it('should not register parallel option metadata when option is not provided', () => {
+    @UseGuards(Guard)
+    class TestNoParallel {}
+    
+    const metadata = Reflect.getMetadata(GUARDS_OPTION_METADATA, TestNoParallel);
+    expect(metadata).to.be.undefined;
+  });
+
+  it('should accumulate parallel option metadata for multiple decorators', () => {
+    class TestMultipleParallel {
+      @UseGuards(Guard, { parallel: true })
+      @UseGuards(Guard, { parallel: true })
+      public static test() {}
+    }
+    const metadata = Reflect.getMetadata(GUARDS_OPTION_METADATA, TestMultipleParallel.test);
+    expect(metadata).to.be.eql([true, true]);
   });
 });
